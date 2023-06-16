7a491300b81e9aab4acbf2ce36ee0275d057cd21
Bright Adams <brightadams58@gmail.com>
2023-06-16 07:28:24 +0000

    scroll top bug when search param changes bug

---
+++ utils/index.ts
@@ -0,0 +1,48 @@
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps){
+    const {manufacturer, year, model, limit, fuel} = filters;
+    const headers = {
+		'X-RapidAPI-Key': '80699aeec9msh52ee75b459c2c57p122fb6jsnadff1ac6a4d8',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla?make=${manufacturer}&year=${year}&model=${model}&fuel_type=${fuel}&limit=${limit}`,{
+        headers
+    });
+
+    const result = await response.json();
+
+    return result
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+  
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+  
+    //return `${url}`;
+    return "https://source.unsplash.com/random"
+  } 
